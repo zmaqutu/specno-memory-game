70a8d35bee3b6c14119a72e7fd7304384a83388c
Zongo Maqutu <zmaqutu@gmail.com>
2024-02-01 23:36:00 +0200

    WIP: installed three'js library and started work ok GameBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PlayerModal from './components/PlayerModal/PlayerModal'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import PlayerModal from './components/PlayerModal/PlayerModal';
+import GameBoard from './components/GameBoard/GameBoard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="font-sans">
@@ -22,7 +23,8 @@ function App() {
         </button>
       </nav>
 
-      <PlayerModal />
+      {/* <PlayerModal /> */}
+      <GameBoard />
     </div>
   )
 }
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useRef} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
 
 function GameBoard() {
+
+  const width = window.innerWidth;
+	const height = window.innerHeight;
+  const cameraPosition = useRef([0,0,275]);
+
+
   return (
-    <div>GameBoard</div>
+    <div className="grid grid-cols-3 items-center mx-auto max-h-screen w-11/12  mt-auto mb-10 bg-green-500">
+      <div className="bg-red-400">Player 1 Score</div>
+      <div className="bg-green-900">
+        <h1>Canvas</h1>
+        {/* <Canvas orthographic camera = {{ position: cameraPosition.current, fov: 53, aspect: width / height, near: 1, far: 5000 }}>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <mesh>
+            <planeGeometry args={[100, 100, 100]} />
+            <meshStandardMaterial side={THREE.DoubleSide} color={'orange'} />
+          </mesh>
+          <OrbitControls />
+        </Canvas> */}
+      </div>
+      <div className="bg-yellow-400 ">Player 2 Score</div>
+    </div>
   )
 }
 
