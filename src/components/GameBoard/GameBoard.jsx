d99b332f75f5afb22b1fc63b26d823cce0b6c557
Zongo Maqutu <zmaqutu@gmail.com>
2024-02-03 01:44:01 +0200

    replaced three.js canvas with css grid and added card back image

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -4,6 +4,7 @@ import { OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
 import player1Image from '../../assets/images/player1.svg';
 import player2Image from '../../assets/images/player2.svg';
+import cardBack from '../../assets/images/cardBack.svg';
 import Card from '../Card/Card';
 
 function GameBoard() {
@@ -22,22 +23,6 @@ function GameBoard() {
   const cardValues = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
   const suits = ['Hearts','Clubs','Spades','Diamonds'];
 
-  // Calculate the horizontal gap between cards
-  const horizontalGap = (633.265625 - ( cardWidth * numCards)) / (numCards + 1);
-  console.log(horizontalGap);
-  // initializeDeck();
-
-  // memoize positions for each neuron in each layer
-//   const cardPositions = useMemo(() => {
-//     const positions = []
-//     for (let row = -210; row < 210; row += 15) {
-//             for (let col = -210; col < 210; col += 15) {
-//                     positions.push(new THREE.Vector3(col, -1, row))
-//             }
-//     }
-//     return positions
-// }, []);
-
   /**
    * A function to initialize the deck (numbers, suits, and Jokers)
    */
@@ -51,9 +36,11 @@ function GameBoard() {
     console.log(tempDeck);
   }
 
+  const cards = Array.from({ length: 54 }, (_, index) => index + 1);
+
 
   return (
-    <div className="grid grid-cols-6 gap-4 items-center mx-auto h-full mb-10 flex-1 overflow-hidden w-11/12  mt-auto  bg-green-500 font-bold">
+    <div className="grid grid-cols-6 gap-4 items-center mx-auto h-full mb-10 flex-1 w-11/12  mt-auto  bg-green-500 font-bold">
       <div className="bg-red-400">
         <img src={player1Image} alt="Player 1" className="w-full h-auto" />
         <p>Player 1</p>
@@ -62,29 +49,14 @@ function GameBoard() {
           It's your turn
         </button>
       </div>
-        <Canvas className="bg-green-900 col-span-4" orthographic camera = {{ position: cameraPosition.current, fov: 53, aspect: width / height, near: 1, far: 5000 }}>
-          <ambientLight />
-          {/* Horizontal Cards */}
-          <Card cardPosition={[-275,210,10]}/>
-          <Card cardPosition={[-205,210,10]}/>
-          <Card cardPosition={[-135,210,10]}/>
-          <Card cardPosition={[-65,210,10]}/>
-          <Card cardPosition={[5,210,10]}/>
-          <Card cardPosition={[75,210,10]}/>
-          <Card cardPosition={[140,210,10]}/>
-          <Card cardPosition={[207.5,210,10]}/>
-          <Card cardPosition={[275,210,10]}/>
-
-          {/* Vertical Card positions */}
-          <Card cardPosition={[-275,127.5,10]}/>
-          <Card cardPosition={[-275,45,10]}/>
-          <Card cardPosition={[-275,-37.5,10]}/>
-          <Card cardPosition={[-275,-120,10]}/>
-          <Card cardPosition={[-275,-202.5,10]}/>
-
-
-          {/* <OrbitControls /> */}
-        </Canvas>
+      {/* Canvas */}
+        <div className="bg-green-900 col-span-4 grid grid-cols-9 gap-1 place-items-center">
+        {cards.map((card) => (
+          <div key={card} className=" p-1 w-4/6">
+            <img src={cardBack} alt=""  />
+          </div>
+        ))}
+        </div>
       <div className="bg-yellow-400">
         <img src={player2Image} alt="Player 2" className="w-full h-auto" />
         <p>Player 2</p>
