0a57b188d6f9b821c2dfe73cb3e5c2b082dcaeee
Zongo Maqutu <zmaqutu@gmail.com>
2024-02-03 00:34:28 +0200

    three.js card implementation: Before Revert

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,20 @@
 import React, { useRef } from 'react';
 import { Vector3 } from 'three';
 import * as THREE from 'three';
+import {  useThree } from '@react-three/fiber';
+
 
 
 function Card(props) {
 
   // define card Geometry and card material - material will be based on props
   const cardGometry = new THREE.PlaneGeometry(50,70);
-  const cardMaterial = new THREE.MeshStandardMaterial({ color: 'lightblue', side: THREE.DoubleSide});
+  const cardMaterial = new THREE.MeshStandardMaterial({ color: 'black', side: THREE.DoubleSide});
+
+  const {cameram, gl} = useThree();
+  const canvasSize = new THREE.Vector2();
+  // console.log('renderer size', gl.getSize(canvasSize))
+
 
   const cardMesh = new THREE.Mesh(cardGometry,cardMaterial);
 
@@ -15,7 +22,7 @@ function Card(props) {
   const cardRef = useRef();
 
   return (
-    <primitive ref={cardRef} object={cardMesh} position={[-270,210,10]}/>
+    <primitive ref={cardRef} object={cardMesh} position={props.cardPosition}/>
   )
 }
 
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,5 +1,5 @@
 import React, {useRef, useState} from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
 import player1Image from '../../assets/images/player1.svg';
@@ -11,13 +11,32 @@ function GameBoard() {
   const width = window.innerWidth;
 	const height = window.innerHeight;
   const cameraPosition = useRef([0,0,100]);
+  const cardWidth = 50;
+  const cardHeight = 70;
+  const numCards = 9;
 
   const [deck, setDeck] = useState({})
 
+ 
   // a list of possible values for each suit
   const cardValues = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
   const suits = ['Hearts','Clubs','Spades','Diamonds'];
-  initializeDeck();
+
+  // Calculate the horizontal gap between cards
+  const horizontalGap = (633.265625 - ( cardWidth * numCards)) / (numCards + 1);
+  console.log(horizontalGap);
+  // initializeDeck();
+
+  // memoize positions for each neuron in each layer
+//   const cardPositions = useMemo(() => {
+//     const positions = []
+//     for (let row = -210; row < 210; row += 15) {
+//             for (let col = -210; col < 210; col += 15) {
+//                     positions.push(new THREE.Vector3(col, -1, row))
+//             }
+//     }
+//     return positions
+// }, []);
 
   /**
    * A function to initialize the deck (numbers, suits, and Jokers)
@@ -45,7 +64,25 @@ function GameBoard() {
       </div>
         <Canvas className="bg-green-900 col-span-4" orthographic camera = {{ position: cameraPosition.current, fov: 53, aspect: width / height, near: 1, far: 5000 }}>
           <ambientLight />
-          <Card/>
+          {/* Horizontal Cards */}
+          <Card cardPosition={[-275,210,10]}/>
+          <Card cardPosition={[-205,210,10]}/>
+          <Card cardPosition={[-135,210,10]}/>
+          <Card cardPosition={[-65,210,10]}/>
+          <Card cardPosition={[5,210,10]}/>
+          <Card cardPosition={[75,210,10]}/>
+          <Card cardPosition={[140,210,10]}/>
+          <Card cardPosition={[207.5,210,10]}/>
+          <Card cardPosition={[275,210,10]}/>
+
+          {/* Vertical Card positions */}
+          <Card cardPosition={[-275,127.5,10]}/>
+          <Card cardPosition={[-275,45,10]}/>
+          <Card cardPosition={[-275,-37.5,10]}/>
+          <Card cardPosition={[-275,-120,10]}/>
+          <Card cardPosition={[-275,-202.5,10]}/>
+
+
           {/* <OrbitControls /> */}
         </Canvas>
       <div className="bg-yellow-400">
