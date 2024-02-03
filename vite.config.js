80d1003d485810a3585a5f4bbfa34097e4d8fb38
Zongo Maqutu <zmaqutu@gmail.com>
2024-02-03 23:31:00 +0200

    fixed typo causing build error in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/space-memory-game/',
+  base: "/spacno-memory-game/",
   plugins: [react()],
   css: {
     postcss: {
