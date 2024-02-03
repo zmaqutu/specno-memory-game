7b51e85983b538eb750477649b84cb1995c907e1
Zongo Maqutu <zmaqutu@gmail.com>
2024-02-03 23:35:06 +0200

    fix-deploy-bugs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/spacno-memory-game/",
+  base: "/specno-memory-game/",
   plugins: [react()],
   css: {
     postcss: {
