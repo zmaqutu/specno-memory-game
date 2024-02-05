080280d2a73a52cf1200a430ac2013ddf5d36d50
Zongo Maqutu <zmaqutu@gmail.com>
2024-02-06 01:25:20 +0200

    feature - implemented 'Its a Match! animation'

--- src/components/GameBoard/MatchConfetti.jsx
+++ src/components/GameBoard/MatchConfetti.jsx
@@ -1,30 +1,47 @@
 import React, { useEffect } from "react";
 import matchConfetti from "../../assets/images/matchConfetti.svg";
+import Confetti from "react-confetti";
+import { useSpring, a } from "@react-spring/web";
 
 function MatchConfetti({ showConfetti, setShowConfetti }) {
-	console.log('showConfetti', showConfetti);
-	useEffect(() => {
-		if (showConfetti) {
-			const timer = setTimeout(() => {
-				setShowConfetti(false);
-			}
-			, 3000);
-			return () => clearTimeout(timer);
-		}
-	}, [showConfetti]);
+    const { confettiAnimation } = useSpring({
+        confettiAnimation: showConfetti ? 1 : 0,
+        config: { mass: 5, tension: 500, friction: 80, duration: 2000 },
+    });
+    const fadeIn = useSpring({
+        opacity: showConfetti ? 1 : 0,
+        from: { opacity: 0 },
+        onRest: () => {
+            if (!showConfetti) {
+                setShowConfetti(false);
+            }
+        },
+    });
     return (
         <>
-            {showConfetti ? (
-                <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="rounded-l text-xl h-full w-full">
-                        <img
-                            src={matchConfetti}
-                            alt="Match Confetti"
-                            className="h-full w-full object-cover"
+            <a.div
+                style={{
+                    ...fadeIn,
+                    pointerEvents: showConfetti ? "auto" : "none",
+                }}
+                className="absolute inset-0 flex items-center justify-center"
+            >
+                <div className="rounded-l text-xl h-full w-full">
+                    <img
+                        src={matchConfetti}
+                        alt="Match Confetti"
+                        className="h-full w-full object-cover"
+                    />
+                    {showConfetti && (
+                        <Confetti
+                            className="h-full w-full"
+                            recycle={false}
+                            numberOfPieces={800}
+                            onConfettiComplete={() => setShowConfetti(false)}
                         />
-                    </div>
+                    )}
                 </div>
-            ) : null}
+            </a.div>
         </>
     );
 }
